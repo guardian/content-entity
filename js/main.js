c4b22c0da419c6631cd0c5fda584e2b88cb18635
tomrf1 <tom.forbes@theguardian.com>
2016-12-01 13:23:47 +0000

    Bump node version to 0.1.1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 exports.sharedTypes  = require('../generated/gen-nodejs/shared_types');
 exports.gameTypes  = require('../generated/gen-nodejs/game_types');
 exports.restaurantTypes  = require('../generated/gen-nodejs/restaurant_types');
+exports.filmTypes = require('../generated/gen-nodejs/film_types');
+exports.personTypes = require('../generated/gen-nodejs/person_types');
